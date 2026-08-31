883e1939a0b2de5eccb580ee7937fa543e1bb3af
SONU SAINI <sonusaini989732@gmail.com>
2026-08-31 13:42:33 +0530

    Time: 1 ms (99.94%), Space: 77.2 MB (66.46%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curSum=0,max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++) {
+            curSum+=nums[i];
+            max=Math.max(max,curSum);
+            if(curSum<0)
+            curSum=0;
+        }
+        return max;
+    }
+}
